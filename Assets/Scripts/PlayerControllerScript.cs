7af8b473478b5f896c5491687cc996163d3e5581
Chris Tammariello <ctammariello@gmail.com>
2016-02-27 17:27:39 -0800

    modified movement script some, added health UI

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -1,49 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControllerScript : MonoBehaviour {
+public class PlayerControllerScript : MonoBehaviour
+{
 
-	public float moveSpeed;
-	public float jumpHeight;
+    public float moveSpeed;
+    public float jumpHeight;
 
-	Transform myTrans;
-	Rigidbody2D myBody;
+    Transform myTrans;
+    Rigidbody2D myBody;
 
-	public Transform groundCheck;
-	public float groundCheckRadius;
-	public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
 
-	private bool isGrounded = false;
-	private bool doubleJumped;
+    private bool isGrounded = false;
+    private bool doubleJump = false;
 
-	void Start () {
-		myBody = this.GetComponent<Rigidbody2D>();
-		myTrans = this.transform;
-	}
+    void Start()
+    {
+        myBody = this.GetComponent<Rigidbody2D>();
+    }
 
-	void FixedUpdate(){
-			Move(Input.GetAxisRaw("Horizontal"));
-			if(isGrounded){
-				doubleJumped = false;
-			}
+    void FixedUpdate()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        myBody.velocity = new Vector2(move * moveSpeed, myBody.velocity.y);
+        if (isGrounded)
+        {
+            doubleJump = false;
+        }
+      
+    }
 
-			if(Input.GetButtonDown("Jump") && isGrounded){
-				Jump();
-			}
-			if(Input.GetButtonDown("Jump") && !doubleJumped && !isGrounded){
-				Jump();
-				doubleJumped = true;
-			}
-			isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || !doubleJump))
+        {
+            Jump();
+            if(!isGrounded && !doubleJump)
+            {
+                doubleJump = true;
+            }
+        }
+    }
 
-	public void Move(float horizontalInput){
-		Vector2 moveVel = myBody.velocity;
-		moveVel.x = horizontalInput * moveSpeed;
-		myBody.velocity = moveVel;
-	}
+    public void Jump()
+    {
+        myBody.AddForce(new Vector2(0, jumpHeight));
+    }
 
-	public void Jump(){
-		myBody.velocity += jumpHeight * Vector2.up;
-	}
 }
